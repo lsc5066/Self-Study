fd467438e2a1f258653e8276deba834fd63855ad
9noeS <leesc332@gmail.com>
2021-01-23 10:15:50 +0900

    Ch7_Exercise

---
+++ C/Ch7_Exercise/3.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//
+//// 3
+//int main() {
+//	int i, input, sum = 0;
+//	
+//	printf("1에서부터의 합을 구하고 싶은 양의 정수를 입력해주세요: ");
+//	scanf("%d", &input);
+//
+//	for (i = 1; i <= input; i++) {
+//		sum += i;
+//	}
+//	printf("1에서부터 %d까지의 합은 %d입니다.", input, sum);
+//	return 0;
+//	}
